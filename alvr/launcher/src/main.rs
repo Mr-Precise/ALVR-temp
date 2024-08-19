d6a97e50320174f72a621da63dd3a21164be8ceb
Precise <3824793+Mr-Precise@users.noreply.github.com>
2024-08-19 15:52:15 +0300

    Switch Launcher to use custom releases

--- alvr/launcher/src/main.rs
+++ alvr/launcher/src/main.rs
@@ -7,8 +7,8 @@ use std::{collections::BTreeMap, io::Cursor, sync::mpsc, thread};
 use ui::Launcher;
 
 pub struct ReleaseChannelsInfo {
-    stable: Vec<ReleaseInfo>,
-    nightly: Vec<ReleaseInfo>,
+    custom: Vec<ReleaseInfo>,
+    official: Vec<ReleaseInfo>,
 }
 
 pub struct Progress {
